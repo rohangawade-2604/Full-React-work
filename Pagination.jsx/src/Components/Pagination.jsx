715562b218e75ffca7627fbf007823a080b25fbc
rngfires <rohangawade2604@gmail.com>
2025-02-05 02:15:18 +0530

    add the pagination.jsx code with component code

--- Pagination.jsx/src/Components/Pagination.jsx
+++ Pagination.jsx/src/Components/Pagination.jsx
@@ -6,13 +6,24 @@ export const Pagination = () => {
 
     const [page, setPage] = useState(null);
     
-    const {store , setStore} = useContext(ContextPag)
+    const showContext = useContext(ContextPag)
 
     const fetchData = async () => {
         try {
-            let Response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+            let Response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=${page}&_page=${showContext.subpage}")
+
+            const header = Response.headers;
+
+            let totalCount = header["x-total-count"]
+            let totalCnts = +totalCount;
+            if (totalCnts) setPage(Math.ceil(totalCnts / 10));
+
+            let dataValue = await Response.data
+            showContext.setStore(dataValue)
+
+
             console.log(Response.data, "api");
-            setStore(Response.data)
+           
         }
         catch (err) {
             console.log(err, "error is been defined");
@@ -21,16 +32,34 @@ export const Pagination = () => {
 
     useEffect(() => {
         fetchData();
-    }, [])
+    }, [showContext.subpage])
 
 
     return (
         <>
-            {store.map((el) => (
+            {showContext.store.map((el) => (
                 <h1 key={el.id}>
                     {el.id}- {el.title}
                 </h1>
             ))}
+
+            <button
+            disabled = {showContext.subpage === 1}
+            onClick={() => {
+                showContext.setSubPage(showContext.subpage - 1)
+            }}>
+                Prev
+            </button>
+
+            <button>{showContext.subpage} show</button>
+
+            <button 
+            disabled = {showContext.subpage === page}
+            onClick={() => {
+                showContext.setSubPage(showContext.subpage +1)
+            }}>
+                Next
+            </button>
         </>
     )
 }
