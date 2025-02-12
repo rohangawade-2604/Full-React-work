f6f58c73cded5cde56a4dc7e10c9970a29db2548
rngfires <rohangawade2604@gmail.com>
2025-02-12 14:42:00 +0530

    done 1% work

--- React-Routing2/src/Context/AuthContext.jsx
+++ React-Routing2/src/Context/AuthContext.jsx
@@ -1,22 +1,23 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import { createContext } from "react";
 
 export const Authcontext = createContext(null)
 
-export const AuthContextProvider = ({ children }) => {
+export const AuthContextProvider = ( props ) => {
     
-    const [Auth ,setAuth] = useState(false);
+    const [auth ,setAuth] = useState(false);
 
     const toggleAuth = () => {
-        setAuth (!Auth)
+        setAuth (!auth)
     }; 
 
     return(
     
-        <Authcontext.Provider value={{ Auth , toggleAuth}}>
-            {children}
+        <Authcontext.Provider value={{ auth , toggleAuth}}>
+            {props.children}
         </Authcontext.Provider>
         
-    )
+    )   
 
 }
