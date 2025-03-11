642595299f7fa0fdaf19483e86389c1ab3f3b60a
rngfires <rohangawade2604@gmail.com>
2025-03-12 03:12:00 +0530

    not done

--- Todo-Context/src/App.jsx
+++ Todo-Context/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <TodoInput />
+      {/* <TodoInput /> */}
     </>
   )
 }
---
+++ Todo-Context/src/IntervalPart/SetInterval.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export const SetInterval = () => {
+
+    const [count , setCount] = useState(0)
+
+        
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
